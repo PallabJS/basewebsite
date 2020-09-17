2d0e030287af90774cbb4cc9d8de18a6847833f2
Pallab Jyoti Singha <40202700+PallabJS@users.noreply.github.com>
2020-09-18 04:34:45 +0530

    finalized the styling and media query

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,40 @@ function handleStickyNav() {
 	}
 }
 
+// TOGGLE NAV BAR
+var toggeled = false;
+$("#navtoggler").addClass("fas fa-bars");
+
 $("#navtoggler").click(() => {
 	$(".navbar_wrapper").toggleClass("navbartoggled");
 	$(".flave_navbar").toggleClass("flave_navbar_show")
+	if (!toggeled) {
+		try {
+			$("#navtoggler").addClass("fa-window-close");
+			$("#navtoggler").removeClass("fas fa-bars");
+			console.log("toggled");
+			toggeled = true;
+		}
+		catch (e) { }
+	}
+	else {
+		try {
+			$("#navtoggler").addClass("fas fa-bars");
+			$("#navtoggler").removeClass("fa-window-close");
+			console.log("shrinked");
+			toggeled = false;
+		}
+		catch (e) { }
+	}
 })
+
+// let screen = window.matchMedia("(max-width: 1000px)");
+// toggleButton(screen);
+// screen.addListener(toggleButton);
+
+// function toggleButton(screen) {
+// 	console.log("matched");
+// 	if (screen.matches) {
+
+// 	}
+// }
